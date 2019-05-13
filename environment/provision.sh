02791c2da053744195d111902f3087f8db676ede
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-05-13 22:42:13 +0100

    added files

---
+++ environment/provision.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo apt-get upgrade -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g pm2
+cd /app
+pm2 start app.js
