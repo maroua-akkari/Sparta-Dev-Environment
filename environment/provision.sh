ceafcbaa6532f1a061652526942118e850845ba8
maroua-akkari <49191068+maroua-akkari@users.noreply.github.com>
2019-05-14 10:09:18 +0100

    Update provision.sh

--- environment/provision.sh
+++ environment/provision.sh
@@ -5,6 +5,7 @@ sudo apt-get install nginx -y
 sudo apt-get upgrade -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo npm install -g pm2
+npm install -g pm2
 cd /app/
+npm install
 pm2 start app.js
